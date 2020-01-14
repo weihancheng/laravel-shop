25d10c9ff20234897256006b21f5c7317d46aba8
weihancheng <2049874524@qq.com>
2020-01-14 15:47:51 +0800

    用户界面 - 商品筛选,排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,15 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\Product;
 
 class ProductsController extends Controller
 {
 
-	public function index()
+	public function index(Request $request)
 	{
-		// query()不加也可以，加 query() 是为了配合 laravel-ide-helper 做代码提示
-		$products = Product::query()->where('on_sale', true)->paginate(16);
-		return view('products.index', compact('products'));
+		// 创建一个查询构造器
+		$builder = Product::query()->where('on_sale', true);
+		// 判断是否有提交search 参数, 如果有就赋值给 $search变量
+		// search 参数用来模糊查询商品
+		if ($search = $request->input('search', '')) {
+			$like = '%'.$search.'%';
+			// 模糊查询商品标题, 商品详情, SKU 标题, SKU描述
+			$builder->where(function ($query) use($like) {
+				$query->where('title', 'like', $like)->orWhere('description', 'like', $like)->orWhereHas('skus', function($query) use ($like) {
+					$query->where('title', 'like', $like)->orWhere('description', 'like', $like);
+				});
+			});
+		}
+
+		// 是否有提交 order参数, 如果有就赋值$order变量
+		// order 参数用来控制商品的排序规则
+		if ($order = $request->input('order', '')) {
+			// 是否是 _asc 或者 _desc结尾
+			if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+				// 如果字符串是['pirce', 'sold_count', 'rating']之一,说明是合法的
+				if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+					// 根据传入的排序值来构造排序参数
+					$builder->orderBy($m[1], $m[2]);
+				}
+			}
+		}
+
+		$products = $builder->paginate(16);
+		return view('products.index', [
+			'products' => $products,
+			'filters' => [
+				'search' => $search,
+				'order' => $order
+			]
+		]);
 	}
 }
