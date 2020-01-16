9c0947e49fe9ee3a5b23ecd0f9cb2408ac6da9c6
weihancheng <2049874524@qq.com>
2020-01-16 16:25:17 +0800

    订单模型

---
+++ app/Models/Order.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    const REFUND_STATUS_PENDING = 'pending';    // pending: 等待, refund: 退款
+    const REFUND_STATUS_APPLIED = 'applied';    // applied: 申请,请求
+    const REFUND_STATUS_PROCESSING = 'processing'; // processing: 处理中
+    const REFUND_STATUS_SUCCESS = 'success'; // 成功的
+    const REFUND_STATUS_FAILED = 'failed'; // 失败的
+
+    const SHIP_STATUS_PENDING = 'pending';  // ship: 运送, pending: 等待
+    const SHIP_STATUS_DELIVERED = 'delivered'; // delivered: 运输
+    const SHIP_STATUS_RECEIVED = 'received';  // received: 收到
+
+	public static $refundStatusMap = [
+		self::REFUND_STATUS_PENDING => '未退款',
+		self::REFUND_STATUS_APPLIED => '已申请退款',
+		self::REFUND_STATUS_PROCESSING => '退款中',
+		self::REFUND_STATUS_SUCCESS => '退款成功',
+		self::REFUND_STATUS_FAILED => '退款失败',
+	];
+
+	public static $shipStatusMap = [
+		self::SHIP_STATUS_PENDING   => '未发货',
+		self::SHIP_STATUS_DELIVERED => '已发货',
+		self::SHIP_STATUS_RECEIVED  => '已收货',
+	];
+
+	protected $casts = [
+		'closed' => 'boolean',
+		'reviewed' => 'boolean',
+		'address' => 'json',
+		'ship_data' => 'json',
+		'extra' => 'json',
+	];
+
+	protected $dates = [
+		'paid_at'
+	];
+
+	protected static function boot()
+	{
+		parent::boot();
+		// 监听模型创建事件, 在写入数据库前触发
+		static::creating(function ($model) {
+			// 如果模型的 no 指端为空
+			if (!$model->no) {
+				// 调用 findAvailableNo
+				$model->no = static::findAvailableNo();
+				// 如果生成失败,则终止创建订单
+				if (!$model->no) {
+					return false;
+				}
+			}
+		});
+	}
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	public function items()
+	{
+		return $this->hasMany(OrderItem::class);
+	}
+
+	public static function findAvailableNo()
+	{
+		// 订单流水号前缀
+		$prefix = date('YmdHis');
+		for ($i = 0; $i < 10; $i ++) {
+			// 随机生成 6位数字
+			$no = $prefix . str_pad(random_int(0, 999999), 6, '0', STR_PAD_LEFT);
+			// 判断是否已经存在
+			if (!static::query()->where('no', $no)->exists()) {
+				return $no;
+			}
+		}
+		\Log::warning('find order no failed');
+		return false;
+	}
+}
